89f970942ff641a9ae20639acafacbc31e705e2f
Alex Coelho <alexsoncoelho21@hotmail.com>
2022-08-23 20:15:18 -0400

    Fix state for selectedRows on truckload form

--- react-app/src/orders/components/TruckLoadForm.js
+++ react-app/src/orders/components/TruckLoadForm.js
@@ -1,28 +1,24 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useHistory } from "react-router";
 import PropTypes from "prop-types";
 import { useState } from "react";
 import axios from "axios";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import filterFactory, {
-  selectFilter, Comparator
+  selectFilter,
+  Comparator,
 } from "react-bootstrap-table2-filter";
-import { useReactToPrint } from "react-to-print";
+import { useReactToPrint, onBeforeGetContent } from "react-to-print";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 export const TruckLoadForm = () => {
-  
   const componentRef = useRef();
-  const [selectedRows, setSelectedRows] = useState([])
-  const [isRowSelected, setIsRowSelected] = useState(false)
-  
+  const [selectedRows, setSelectedRows] = useState([]);
 
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
@@ -35,7 +31,6 @@ export const TruckLoadForm = () => {
 
   const getData = () => {
     axios(`${process.env.REACT_APP_API_URL}/orders`).then((res) => {
-      console.log(res.data);
       setData(res.data);
     });
   };
@@ -52,15 +47,17 @@ export const TruckLoadForm = () => {
     roduction: "In Production",
   };
 
-  const dateFormatter=(data,row)=>{
-    return<>{new Date(
-      data
-    ).toLocaleDateString("en-us", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    })}</>
-  }
+  const dateFormatter = (data, row) => {
+    return (
+      <>
+        {new Date(data).toLocaleDateString("en-us", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+      </>
+    );
+  };
 
   const columns = [
     {
@@ -90,7 +87,7 @@ export const TruckLoadForm = () => {
       sort: true,
       filter: selectFilter({
         options: selectOptions,
-        comparator: Comparator.LIKE
+        comparator: Comparator.LIKE,
       }),
     },
     {
@@ -99,46 +96,38 @@ export const TruckLoadForm = () => {
       sort: true,
       filter: selectFilter({
         options: selectStatus,
-        comparator: Comparator.LIKE
+        comparator: Comparator.LIKE,
       }),
     },
   ];
 
-  // on selects
-
   const handleOnSelect = (row, isSelect, rowIndex, e) => {
-    setSelectedRows([...selectedRows, row])
-    setIsRowSelected(() => isSelect)
-    return true
-  }
+    if (isSelect) {
+      setSelectedRows([...selectedRows, row]);
+    } else {
+      const newSelectedRows = selectedRows.filter(
+        (currentRow) => currentRow.id !== row.id
+      );
+      setSelectedRows(newSelectedRows);
+    }
+    return true;
+  };
 
   const handleOnSelectAll = (isSelect, rows) => {
-    setIsRowSelected(isSelect)
-    setSelectedRows(rows)
-  }
-
-  // select row from bootstrap table
+    if (!isSelect) setSelectedRows([]);
+    else {
+      setSelectedRows(rows);
+    }
+  };
 
   const selectRow = {
-    mode: 'checkbox',
+    mode: "checkbox",
     clickToSelect: true,
     // hideSelectColumn: true,
-    bgColor: '#55D6BE',
+    bgColor: "#55D6BE",
     onSelectAll: handleOnSelectAll,
     onSelect: handleOnSelect,
-    }
-  
-
-
-  // send email to those on the selected
-
-  // selectedRows.forEach(row => {
-  //   sendEmail(row)
-  // }) 
-
-
-
-    
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -160,82 +149,91 @@ export const TruckLoadForm = () => {
           console.log(error.text);
         }
       );
-    alert ("Your Emails have been sent!")
+    alert("Your Emails have been sent!");
   };
 
+  const handlePrintPackingSlips = async () => {
+    selectedRows.forEach((row) =>
+      window.open(`profile/user/${row.id}/orderdetails/packing-slip`, "_blank")
+    );
+  };
 
   return (
     <>
-    
-        {/* <div className='w-100 h-100 p-2 rounded-3'> */}
-          <div className='p-3' >
-            <div>
-              <Container>
-                <Row>
-                  <Col>
-                    <Button
-                        onClick={sendEmail}
-                        variant='success'
-                        className='mb-3'
-                      >
-                        Notify Out for Delivery
-                    </Button>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-            <div ref={componentRef}> 
-              <div>
-                <Row className='justify-content-between'>
-                  <Col>
-                    <img
-                      alt=''
-                      src='/./../Step-Solution-Logo-Dark.png'
-                      width='330'
-                      height='auto'
-                      className='d-inline-block align-center'
-                    />{" "}
-                  </Col>
-
-                  <Col>
-                    <h1 className='text-end align-text-bottom'>
-                      <strong>Truck</strong>Load
-                    </h1>
-                  </Col>
-                </Row>
-              </div>
-
-                {/* Dashboard content */}
-
-              <div className=''>
-                  <BootstrapTable
-                    keyField='id'
-                    data={data}
-                    columns={columns}
-                    selectRow={ selectRow }
-                    striped
-                    hover
-                    condensed
-                    pagination={paginationFactory()}
-                    filter={filterFactory()}
-                  ></BootstrapTable>
-              </div>
-            </div>
-
-          </div>
-        
+      {/* <div className='w-100 h-100 p-2 rounded-3'> */}
+      <div className='p-3'>
+        <div>
           <Container>
-            <Col className='mb-5 text-center'>
-              <Button
-                onClick={handlePrint}
-                variant="primary" type='submit' className="printer-btn shadow col-6"
-              >
-                print this view
-              </Button>
-            </Col>
+            <Row md={4}>
+              <Col>
+                <Button onClick={sendEmail} variant='success' className='mb-3'>
+                  Notify Out for Delivery
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  onClick={handlePrintPackingSlips}
+                  variant='success'
+                  className='mb-3'
+                >
+                  Print Packing Slips
+                </Button>
+              </Col>
+            </Row>
           </Container>
-        {/* </div> */}
- 
+        </div>
+        <div ref={componentRef}>
+          <div>
+            <Row className='justify-content-between'>
+              <Col>
+                <img
+                  alt=''
+                  src='/./../Step-Solution-Logo-Dark.png'
+                  width='330'
+                  height='auto'
+                  className='d-inline-block align-center'
+                />{" "}
+              </Col>
+
+              <Col>
+                <h1 className='text-end align-text-bottom'>
+                  <strong>Truck</strong>Load
+                </h1>
+              </Col>
+            </Row>
+          </div>
+
+          {/* Dashboard content */}
+
+          <div className=''>
+            <BootstrapTable
+              keyField='id'
+              data={data}
+              columns={columns}
+              selectRow={selectRow}
+              striped
+              hover
+              condensed
+              pagination={paginationFactory()}
+              filter={filterFactory()}
+            ></BootstrapTable>
+          </div>
+        </div>
+      </div>
+
+      <Container>
+        <Col className='mb-5 text-center'>
+          <Button
+            onClick={handlePrint}
+            variant='primary'
+            type='submit'
+            className='printer-btn shadow col-6'
+          >
+            print this view
+          </Button>
+        </Col>
+      </Container>
+      {/* </div> */}
     </>
   );
 };
